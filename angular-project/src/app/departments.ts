a2bd497c5fd340dd5c924317bb8aeab572701a26
Angel Hadzhiev <derkraizer@gmail.com>
2018-09-24 22:45:01 +0200

    final changes on departments

--- angular-project/src/app/departments.ts
+++ angular-project/src/app/departments.ts
@@ -1,10 +1,10 @@
 import { Department} from './department';
 
-export const Departments: Department[] = [
-{id:1 , name:'ASAI',location:'Eindhoven', menId:101200 },
-{id:2 , name:'AMC',location:'Eindhoven', menId:101201 },
-{id:3 , name:'BACID',location:'Amsterdam',menId:101202 },
-{id:4 , name:'BEIM',location:'Amsterdam',menId:101203 },
-{id:5, name:'BISSNI',location:'Eindhoven',menId:101204 },
-{id:5, name:'AMC', location:'The Hague',menId:101205}
-]
+
+export const Departments: Department[] =[];
+Departments.push(new Department( 'ASAI', 'Eindhoven' ));
+Departments.push(new Department('AMC', 'Eindhoven'));
+Departments.push(new Department('BACID', 'Amsterdam' ));
+Departments.push(new Department('BEIM', 'Amsterdam'));
+Departments.push(new Department('BISSNI', 'Eindhoven'));
+Departments.push(new Department('AMC', 'The Hague'));
